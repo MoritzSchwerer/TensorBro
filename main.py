29ce36b16f782e51a123cc86aec533f33aac18d6
MoritzSchwerer <moritz.schwerer@gmx.de>
2023-12-20 11:18:34 +0100

    succesfully run elemenwise ops in clang backend

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
-from src.lazy import LazyBuffer, LazyOp
-from src.ops import LoadOps
+from src.lazy import LazyBuffer
+from src.linearizer import linearize
+from src.ops import UnaryOps
 
-# import numpy as np
+
+b1 = LazyBuffer.rand((10, ), 'CPU', arg=1)
 
 
-if __name__ == '__main__':
-    op1 = LazyOp(LoadOps.Rand, ())
-    op2 = LazyOp(LoadOps.Rand, ())
-    op3 = LazyOp(LoadOps.Rand, ())
+ops = [
+    UnaryOps.NEG,
+    UnaryOps.SIN,
+    UnaryOps.SQRT,
+    UnaryOps.EXP2,
+    UnaryOps.LOG2,
+]
+for op in ops:
+    print("="*99)
+    print(op)
+    b4 = b1.elementwise(op)
+    sch = b4.schedule()
+    runner = linearize(sch)
+    runner()
 
-    l1 = LazyBuffer(op1, 'CPU', (10, 10))
-    l2 = LazyBuffer(op2, 'CPU', (10, 10))
-    l3 = LazyBuffer(op2, 'CPU', (10, 10))
-    l4 = l1 * l2 + l3
-    sch = l4.schedule()
-    for s in reversed(list(sch)):
-        print(s)
+    print('[', end='')
+    for j in range(10):
+        print(b4.base[+j], end=', ')
+    print(']')
+
+print("="*99)
+print("base value")
+print('[', end='')
+for j in range(10):
+    print(b1.base[j], end=', ')
+print(']')
