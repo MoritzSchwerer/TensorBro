ec0cc79e76ab21ece7efe719265e8429020b9a2b
MoritzSchwerer <moritz.schwerer@gmx.de>
2023-12-20 11:19:22 +0100

    added print_tree utlity to visualize schedule

--- src/linearizer.py
+++ src/linearizer.py
@@ -3,18 +3,37 @@ import ctypes as c
 from typing import List, Callable, Union, Tuple, TYPE_CHECKING
 from dataclasses import dataclass
 
-# TODO: This dependency is sucks
+# TODO: This dependency sucks
 from .code_gen.clang import CProgram
 
+from .lazy import LazyOp
+
 if TYPE_CHECKING:
-    from .lazy import LazyOp, LazyBuffer
+    from .lazy import LazyBuffer
+
 
 
 @dataclass(frozen=True)
 class ScheduleItem:
-    op: 'LazyOp'
+    op: LazyOp
     target: 'LazyBuffer'
-    srcs: Tuple[Union['LazyBuffer', 'LazyOp'], ...]
+    srcs: Tuple[Union['LazyBuffer', LazyOp], ...]
+
+
+def print_tree(si: ScheduleItem, depth=0):
+    if isinstance(si, ScheduleItem):
+        print(
+            '    ' * depth + str(si.op.op.name),
+            '' if si.op.arg is None else str(si.op.arg),
+        )
+        for src in si.op.srcs:
+            print_tree(src, depth=depth + 1)
+    elif isinstance(si, LazyOp):
+        print('   ' * depth + str(si.op.name), '' if si.arg is None else str(si.arg))
+        for src in si.srcs:
+            print_tree(src, depth=depth + 1)
+    else:
+        print('    ' * depth + str(type(si).__name__))
 
 
 def linearize(schedule: List[ScheduleItem]) -> Callable:
