d53b539b9e39f11d8515d83b60237ead72c99579
MoritzSchwerer <moritz.schwerer@gmx.de>
2023-12-27 14:22:38 +0100

    improved expandded tensor ops experiment

--- experiments/expanded_tensors.c
+++ experiments/expanded_tensors.c
@@ -1,29 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int calc_index(int *stride, int *shape, int *index) {
-    int idx = 0;
-    int mul = 1;
-    for (int i = 2; i >= 0; i--) {
-        idx += index[i] / stride[i] * mul;
-        mul *= shape[i];
-        // mul *= (i > 0) ? shape[i] : 1;
-    }
-    return idx;
-}
-
 void multiply_tensors(int *data1, int *stride1, int *data2, int *stride2, int *result_data) {
-    int index[] = {0,0,0};
     int shape[] = {2, 2, 3};
-    for (int i = 0; i < 2; i++) { // first dim
-        index[0] = i;
-        for (int j = 0; j < 2; j++) {  // second dim
-            index[1] = j;
-            for (int k = 0; k < 3; k++) { // third dim
-                index[2] = k;
-                int idx1 = i*3*2 + j*3 + k;
-                int idx2 = k /stride2[2] + (j / stride2[1]) * 3 + (i / stride2[0]) * 3 * 2;
-                printf("%d, %d\n", idx2, calc_index(stride2, shape, index));
+    for (int i = 0; i < shape[0]; i++) {
+        for (int j = 0; j < shape[1]; j++) {
+            for (int k = 0; k < shape[2]; k++) {
+                int idx1 = (i / stride1[0]) * shape[1] * shape[2] + (j / stride1[1]) * shape[2] + (k / stride1[2]);
+                int idx2 = (i / stride2[0]) * shape[1] * shape[2] + (j / stride2[1]) * shape[2] + (k / stride2[2]);
                 result_data[idx1] = data1[idx1] * data2[idx2];
             }
         }
